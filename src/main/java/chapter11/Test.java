149c89b38397521a7bb80eb8461ea14b61264c1f
qwang1990 <526186564@qq.com>
2017-02-14 21:09:09 +0800

    chapter12

---
+++ src/main/java/chapter11/Test.java
@@ -0,0 +1,28 @@
+package chapter11;
+
+/**
+ * Created by wangqi on 17/2/10.
+ */
+public class Test {
+    public static final int NUM = 15000;
+
+    public static int doubleValue(int i) {
+        // 这个空循环用于后面演示JIT代码优化过程
+        for(int j=0; j<100000; j++);
+        return i * 2;
+    }
+
+    public static long calcSum() {
+        long sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += doubleValue(i);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < NUM; i++) {
+            calcSum();
+        }
+    }
+}
---
+++ src/main/java/chapter12/Singleton.java
@@ -0,0 +1,23 @@
+package chapter12;
+
+/**
+ * Created by wangqi on 17/2/9.
+ */
+public class Singleton {
+    private volatile static Singleton instance;
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton.getInstance();
+    }
+}
